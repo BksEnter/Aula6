394456af34b6502ea58ff47ec42bcb1caf80f990
Bruno <brunokoeke@gmail.com>
2025-03-31 08:59:29 -0300

    Variaveis Js

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+//DECLARAÇÕES E VARIAVEIS
+
+var nome="fiap";
+console.log(nome);
+
+let idade = 19;
+console.log(idade)
+
+const sobrenome="Fiainho";
+console.log(sobrenome);
+
+//Declarand uma variavel indefinida
+let nome1;
+console.log(nome1)
